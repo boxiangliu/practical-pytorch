a5acf27d67751e6f1fb7c1b8acc8ad23f877b41d
Boxiang Liu <jollier.liu@gmail.com>
2020-11-10 16:26:36 -0800

    refactered train.py

--- char-rnn-classification/train.py
+++ char-rnn-classification/train.py
@@ -4,31 +4,34 @@ from model import *
 import random
 import time
 import math
+import pickle
 
 n_hidden = 128
 n_epochs = 100000
 print_every = 5000
 plot_every = 1000
-learning_rate = 0.005 # If you set this too high, it might explode. If too low, it might not learn
+# If you set this too high, it might explode. If too low, it might not learn
+learning_rate = 0.005
+
 
 def categoryFromOutput(output):
-    top_n, top_i = output.data.topk(1) # Tensor out of Variable with .data
+    top_n, top_i = output.data.topk(1)  # Tensor out of Variable with .data
     category_i = top_i[0][0]
     return all_categories[category_i], category_i
+
 
 def randomChoice(l):
     return l[random.randint(0, len(l) - 1)]
 
-def randomTrainingPair():
+
+def randomTrainingExample():
     category = randomChoice(all_categories)
     line = randomChoice(category_lines[category])
-    category_tensor = Variable(torch.LongTensor([all_categories.index(category)]))
+    category_tensor = Variable(torch.LongTensor(
+        [all_categories.index(category)]))
     line_tensor = Variable(lineToTensor(line))
     return category, line, category_tensor, line_tensor
 
-rnn = RNN(n_letters, n_hidden, n_categories)
-optimizer = torch.optim.SGD(rnn.parameters(), lr=learning_rate)
-criterion = nn.NLLLoss()
 
 def train(category_tensor, line_tensor):
     hidden = rnn.initHidden()
@@ -43,11 +46,8 @@ def train(category_tensor, line_tensor):
     optimizer.step()
 
     # return output, loss.data[0]
-    return output, loss.data # Boxiang 
+    return output, loss.data  # Boxiang
 
-# Keep track of losses for plotting
-current_loss = 0
-all_losses = []
 
 def timeSince(since):
     now = time.time()
@@ -56,23 +56,39 @@ def timeSince(since):
     s -= m * 60
     return '%dm %ds' % (m, s)
 
-start = time.time()
 
-for epoch in range(1, n_epochs + 1):
-    category, line, category_tensor, line_tensor = randomTrainingPair()
-    output, loss = train(category_tensor, line_tensor)
-    current_loss += loss
+def main():
+    rnn = RNN(n_letters, n_hidden, n_categories)
+    optimizer = torch.optim.SGD(rnn.parameters(), lr=learning_rate)
+    criterion = nn.NLLLoss()
 
-    # Print epoch number, loss, name and guess
-    if epoch % print_every == 0:
-        guess, guess_i = categoryFromOutput(output)
-        correct = '✓' if guess == category else '✗ (%s)' % category
-        print('%d %d%% (%s) %.4f %s / %s %s' % (epoch, epoch / n_epochs * 100, timeSince(start), loss, line, guess, correct))
+    # Keep track of losses for plotting
+    current_loss = 0
+    all_losses = []
 
-    # Add current loss avg to list of losses
-    if epoch % plot_every == 0:
-        all_losses.append(current_loss / plot_every)
-        current_loss = 0
+    start = time.time()
 
-torch.save(rnn, 'char-rnn-classification.pt')
+    for epoch in range(1, n_epochs + 1):
+        category, line, category_tensor, line_tensor = randomTrainingExample()
+        output, loss = train(category_tensor, line_tensor)
+        current_loss += loss
 
+        # Print epoch number, loss, name and guess
+        if epoch % print_every == 0:
+            guess, guess_i = categoryFromOutput(output)
+            correct = '✓' if guess == category else '✗ (%s)' % category
+            print('%d %d%% (%s) %.4f %s / %s %s' % (epoch, epoch /
+                                                    n_epochs * 100, timeSince(start), loss, line, guess, correct))
+
+        # Add current loss avg to list of losses
+        if epoch % plot_every == 0:
+            all_losses.append(current_loss / plot_every)
+            current_loss = 0
+
+    torch.save(rnn, 'char-rnn-classification.pt')
+    with open("./all_losses.pkl", "wb") as fout:
+        pickle.dump(all_losses, fout)
+
+
+if __name__ == "__main__":
+    main()
